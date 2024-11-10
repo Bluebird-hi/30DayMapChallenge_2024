1231f1d4690a128e7c4d21ac1b6e6014661e379c
Luming Xu <xuluming0820@outlook.com>
2024-11-10 03:55:44 -0500

    07

--- 07-Vintage Style/07-Vintage Style.R
+++ 07-Vintage Style/07-Vintage Style.R
@@ -12,7 +12,7 @@ library(ggtext)
 library(magick)
 
 # define the spatial extent to OpenStreetMap
-lat1 <- 22.855; lat2 <- 22.997; lon1 <- 113.476; lon2 <- 113.59202508003357
+lat1 <- 22.855; lat2 <- 22.998; lon1 <- 113.476; lon2 <- 113.59202508003357
 # bbbox
 bbox <- c(xmin = lon1, ymin = lat1, xmax = lon2, ymax = lat2)
 
@@ -50,11 +50,11 @@ villages <- landuse$osm_polygons %>%
   )) %>%
   filter(name == "island",
          !osm_id %in% c(449809649, 449809658, 921993169, 921990428, 925604363, 539855632, 	
-                        1289841871))
+                        1289841871, 539855639))
 
 # Base Water
 ## Data source(maptiler) https://data.maptiler.com/downloads/dataset/osm/asia/china/guangzhou/#7.29/23.259/113.576
-sf_data <- st_read("07-Vintage Style/data/osm-2020-02-10-v3.11_china_guangzhou.mbtiles")
+sf_data <- st_read("data/osm-2020-02-10-v3.11_china_guangzhou.mbtiles")
 sf_data <- st_transform(sf_data, crs = 4326)
 # ggplot(data = sf_data) +
 #   geom_sf(fill = "#7AB2D3", color = NA)+
@@ -66,7 +66,7 @@ sf_data <- st_transform(sf_data, crs = 4326)
 #### Plot ####
 
 plot <- ggplot() +
-  geom_sf(data = sf_data, fill = "#9EBFAA", color = NA, alpha = .8) +
+  geom_sf(data = sf_data, fill = "#9EBFAA", color = NA) +
   
   # Add natural with a warm beige color
   geom_sf(data = seagull, fill = "#FFF7D8", color = NA) +
@@ -76,22 +76,22 @@ plot <- ggplot() +
   
   
   # Add waterways with a light teal color
-  geom_sf(data = waterways2, color = "#9EBFAA", lwd = .5, alpha = .8) +
+  geom_sf(data = waterways2, color = "#9EBFAA", lwd = .5) +
   
   # Bouding Box
   coord_sf(xlim = c(bbox[1], bbox[3]), ylim = c(bbox[2], bbox[4])) +
 
   # Customize map background to a cream color
   theme_void() +
-  theme(panel.background = element_rect(fill = alpha("#F5E4C3", 0.9), color = NA)) 
+  theme(panel.background = element_rect(fill = "#F5E4C3", color = NA)) 
 
 
 
 
 #### Plot: save ####
 
-ggsave(filename = "07-Vintage Style/Outputs/map.png", plot = plot, height = 10, width = 7.5, units = "in", dpi = 300)
-img <- image_read("07-Vintage Style/Outputs/map.png")
+ggsave(filename = "Outputs/map.png", plot = plot, height = 10, width = 7.5, units = "in", dpi = 300)
+img <- image_read("Outputs/map.png")
 img_final <- image_annotate(img, "Seagull Island", 
                             size = 200, color = "#6B4226",
                             font = "serif bold",
@@ -111,4 +111,4 @@ img_final <- image_annotate(img_final,
                             font = "poppins",
                             gravity = "south",
                             location = "+50+50")
-image_write(img_final, path = "07-Vintage Style/Outputs/07-Luming-Vintage.png")
+image_write(img_final, path = "Outputs/07-Luming-Vintage.png")
